6621714e4a19d4a7f06b3aae870a53109c9a350c
TemptationWithin <75949800+TemptationWithin@users.noreply.github.com>
2021-01-20 23:52:00 +0300

    Delete src/com/homework_lesson_8_task1 directory

--- src/com/homework_lesson_8_task1/exception/InvalidCodeFormat.java
+++
@@ -1,9 +0,0 @@
-package com.homework_lesson_8_task1.exception;
-
-import java.io.IOException;
-
-public class InvalidCodeFormat extends IOException {
-    public InvalidCodeFormat(String message) {
-        super(message);
-    }
-}
